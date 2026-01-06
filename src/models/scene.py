e3c6ef14633e6cfd3aa91267448e499e1e538539
dimitri <dimitri.calmand@epita.fr>
2026-01-06 10:36:59 +0100

    feat: scene

--- src/models/scene.py
+++ src/models/scene.py
@@ -1,5 +1,43 @@
-class Scene:
+from typing import Optional, List, Union
+import torch
+import torch.nn as nn
+from src.models.gaussians import GaussianSet
+from src.models.volumetric import VolumetricGaussianSet
+
+
+class Scene(nn.Module):
+    
     def __init__(self):
-        self.surface_gaussians
-        self.volume_gaussians
+        super().__init__()
+        self.surface_gaussians: List[GaussianSet] = []
+        self.volume_gaussians: List[VolumetricGaussianSet] = []
+        self.scene_bounds = None
+        self.device = torch.device("cuda" if torch.cuda.is_available() else 
+                                  ("mps" if torch.backends.mps.is_available() else "cpu"))
+    
+    def clear(self):
+        self.surface_gaussians.clear()
+        self.volume_gaussians.clear()
+    
+    def train(self, mode: bool = True):
+        super().train(mode)
+        for gs in self.surface_gaussians:
+            gs.train(mode)
+        for vgs in self.volume_gaussians:
+            vgs.train(mode)
+        return self
+    
+    def eval(self):
+        return self.train(False)
+    
+    def parameters(self):
+        params = []
+        for gs in self.surface_gaussians:
+            params.extend(gs.parameters())
+        for vgs in self.volume_gaussians:
+            params.extend(vgs.parameters())
+        return params
+    
+    
+
 
